9f3b3c7652345c5ee058f403a230b4638c7c4758
Rei-Ashine <ashine.rei.74z@st.kyoto-u.ac.jp>
2021-04-29 23:11:06 +0900

    Add scripts for Validation

---
+++ Level2/DiseaseGenomeAnalysis_ver2/330_SRY-coverage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+id=DRR006760
+bam=${id}.sort.bam
+alb="chr4:73404349-73421412"
+sry="chrY:2786855-2787699"
+outalb=alb.depth.txt
+outsry=sry.depth.txt
+
+samtools depth -r ${alb} ${bam} > ${outalb}
+samtools depth -r ${sry} ${bam} > ${outsry}
+
+echo "mean depth of the ALB gene on chromosome 4:"
+cat ${outalb} | awk '{sum+=$3} END {print sum/NR}'
+
+echo "mean depth of the SRY gene on chromosome Y:"
+cat ${outsry} | awk '{sum+=$3} END {print sum/NR}'
